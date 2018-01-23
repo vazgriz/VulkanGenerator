096fb5dab48fe4d9ee79aa35f9f915b2df1ede29
Unknown <vazgriz@gmail.com>
2018-01-22 21:42:13 -0600

    Add CppGenerator

---
+++ CppGenerator/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+using SpecReader;
+
+namespace CppGenerator {
+    public class Generator {
+        DateTime time;
+        Spec spec;
+
+        public Generator(Spec spec) {
+            this.spec = spec;
+            time = DateTime.Now;
+        }
+
+        public void WriteEnums(string path) {
+            path = Path.Combine(path, "enums.h");
+
+            using (var writer = File.CreateText(path)) {
+                writer.WriteLine("//auto generated on {0}", time.ToString());
+
+                foreach (var name in spec.EnumMap.Keys) {
+                    writer.WriteLine("enum class {0} {{", name);
+
+                    var e = spec.EnumMap[name];
+
+                    foreach (var value in e.Values) {
+                        writer.WriteLine("    {0},", value.Name);
+                    }
+
+                    writer.WriteLine("};\n");
+                }
+            }
+        }
+    }
+}
---
+++ CppGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+
+using SpecReader;
+
+namespace CppGenerator {
+    class Program {
+        static void Main(string[] args) {
+            string output = "output";
+
+            if (!Directory.Exists(output)) Directory.CreateDirectory(output);
+
+            Spec spec;
+            using (var reader = File.Open("vk.xml", FileMode.Open)) {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(reader);
+                spec = new Spec(doc, 1, 0, null);
+            }
+
+            Generator g = new Generator(spec);
+            g.WriteEnums(output);
+        }
+    }
+}
